c08267756798da8634727e5c99af04a084240994
DhanasekarA-18 <dhanasekard848@gmail.com>
2023-03-12 15:27:14 +0530

    test-fix

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ app.use(express.json());
 app.get('/',(req,res)=>{
      res.status(200).json({msg:"server running"})
  })
- 
+ app.get("/test",(req,res)=>{
+    res.status(200).send("<h1>hello</h1>")
+ })
  app.use('/*',(req,res)=>{
      res.status(404).json({msg:"Route Not Found"});
  })
- app.get("/test",(req,res)=>{
-    res.status(200).send("<>hello</>")
- })
+
  app.listen(process.env.Port_No||3000,()=>{
      console.log(`Server listining @ ${process.env.Port_No}`);
  })
